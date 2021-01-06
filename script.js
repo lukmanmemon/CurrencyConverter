87bca49653b2f067569d4f771f1333c6e8685f0e
Lukman Memon <52639954+lukmanmemon@users.noreply.github.com>
2021-01-05 23:40:42 -0500

    Added graph displaying historical conversion rate

--- script.js
+++ script.js
@@ -21,15 +21,20 @@ function getUpdatedValue() {
 sel.addEventListener("change", function() {
    currencyType1 = this.value;
    getUpdatedValue();
+   getHistoricalData();
+   plotGraph();
 });
 
 // When currencyType2 changes
 selCopy.addEventListener("change", function() {
    currencyType2 = this.value;
    getUpdatedValue();
+   getHistoricalData();
+   plotGraph();
 });
 
 getUpdatedValue();  
+
 // change input2 text when input1 text changes
 input1.addEventListener("input", function() {
    input2.value = (Math.round(input1.value * latestPrice * 100) / 100).toFixed(2);
@@ -51,3 +56,132 @@ input2.addEventListener("input", function() {
      input1.value = "";
    }
 });
+
+var inputDate1 = document.getElementById("start");
+var inputDate2 = document.getElementById("end");
+
+var today = new Date();
+
+var dd = String("0" + today.getDate()).slice(-2);
+var mm = String("0" + today.getMonth() + 1).slice(-2);
+var yyyy = String(today.getFullYear());
+today = yyyy + '-' + mm + '-' + dd;
+// sets the latest date user can select as today's date
+document.getElementById("end").setAttribute("max", today);
+document.getElementById("start").setAttribute("max", today);
+
+inputDate2.value = today;
+var startDate = inputDate1.value;
+var endDate = inputDate2.value;
+
+function getHistoricalData() {
+   fetch("https://api.exchangeratesapi.io/history?start_at=" + startDate + "&end_at=" + endDate + "&symbols=" + currencyType2 + "&base=" + currencyType1)
+   .then(response => response.json())
+   .then(data => {
+      historicalRate = data.rates;
+      d = [];
+      c = [];
+      for(let date in historicalRate) {
+         d.push(date);
+         currencyValue = historicalRate[date];
+         for(let key in currencyValue) {
+            c.push(currencyValue[key]);
+         }
+      }
+      console.log(d);
+      console.log(c);
+   });
+}
+
+
+
+
+inputDate1.addEventListener("change", function() {
+   startDate = this.value;
+   getHistoricalData();
+   plotGraph();
+});
+
+inputDate2.addEventListener("change", function() {
+   endDate = this.value;
+   getHistoricalData();
+   plotGraph();
+
+});
+
+
+function BuildChart(labels, values, chartTitle) {
+   var data = {
+       labels: labels,
+       datasets: [{
+           label: chartTitle, 
+           data: values,
+           backgroundColor: 'lightgrey',
+           borderColor: 'darkblue',
+           pointBackgroundColor: 'white'
+               
+           
+       }],
+   };
+
+   var ctx = document.getElementById("myChart").getContext('2d');
+   var myChart = new Chart(ctx, {
+       type: 'line',
+       data: data,
+       options: {
+           responsive: false, 
+           maintainAspectRatio: true, 
+           scales: {
+               xAxes: [{
+                       scaleLabel: {
+                       display: true,
+                       labelString: 'Date'
+                   }
+               }],
+               yAxes: [{
+                   scaleLabel: {
+                       display: true,
+                       labelString: 'Currency Rate'
+                   }
+               }]
+           },
+       }
+   });
+
+   return myChart;
+}
+
+function plotGraph() {
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         var data = JSON.parse(this.response);
+         historicalRate = data.rates;
+         d = [];
+         c = [];
+         for(let date in historicalRate) {
+            d.push(date);
+            currencyValue = historicalRate[date];
+            for(let key in currencyValue) {
+            c.push(currencyValue[key]);
+            }
+         }
+         console.log(d);
+         console.log(c);
+
+         // Set dates for graph
+         var labels = d;
+      
+         // Set values for graph
+         var values = c;
+
+         BuildChart(labels, values, "Currency conversion rate");
+      }
+   };
+   xhttp.open("GET", "https://api.exchangeratesapi.io/history?start_at=" + startDate + "&end_at=" + endDate + "&symbols=" + currencyType2 + "&base=" + currencyType1, true);
+   xhttp.send();
+ }
+
+ plotGraph();
+
+
